d4953b6bf6b1b314e42d7f8aac845936086beb43
魏文苑 <14150312+qd-wwy@user.noreply.gitee.com>
2024-04-23 09:58:20 +0800

    初始化仓库

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const customers = () => {
+    return (
+        <p>Customers Page</p>
+    )
+}
+
+export default customers
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+//  谷歌的字体模块，设置字体样式
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({weight:'400',subsets: ['latin']});
